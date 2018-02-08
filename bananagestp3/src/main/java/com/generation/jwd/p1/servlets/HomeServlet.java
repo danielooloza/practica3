aa35a6dac1f2e372490b98fa255d9c13b1154c37
RodrigoMGM1 <rodri.mar93@hotmail.com>
2018-02-08 17:06:06 +0100

    homeservlet works

--- bananagestp3/src/main/java/com/generation/jwd/p1/servlets/HomeServlet.java
+++ bananagestp3/src/main/java/com/generation/jwd/p1/servlets/HomeServlet.java
@@ -5,22 +5,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.sql.DataSource;
-
 import com.generation.jwd.p1.beans.Task;
-import com.generation.jwd.p1.beans.User;
 
 
 @WebServlet("/homeuser")
